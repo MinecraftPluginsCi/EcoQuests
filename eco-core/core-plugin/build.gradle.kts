e1e5ec08692a9a0ad1fc379c5cd9026a02ffe35a
qscbm187531 <1309635304@qq.com>
2024-10-03 11:21:14 +0800

    Update build.gradle.kts

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -7,29 +7,6 @@ dependencies {
     implementation("com.willfp:ecomponent:1.4.1")
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            version = project.version.toString()
-            artifactId = rootProject.name
-
-            artifact(rootProject.tasks.shadowJar.get().archiveFile)
-        }
-    }
-
-    repositories {
-        maven {
-            name = "auxilor"
-            url = uri("https://repo.auxilor.io/repository/maven-releases/")
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
-            }
-        }
-    }
-}
-
 tasks {
     build {
         dependsOn(publishToMavenLocal)
